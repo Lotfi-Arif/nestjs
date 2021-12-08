45407f6c63091acf6b8f9c983bfa5cddd96aa236
Lotfi <Lutfi.arif2799@gmail.com>
2021-12-08 22:54:55 +0800

    Add methods to service and controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Query } from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+
+    constructor(private coursesService: CoursesService){}
+
+    @Get()
+    async getCourses() {
+        const course = await this.coursesService.getCourses();
+    }
+
+    @Get(':courseId')
+    async getCourse(@Param('courseId') courseId){
+        const course = await this.coursesService.getCousrse(courseId);
+        return course;
+    }
+}
--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { resolve } from 'path/posix';
+import { COURSES } from './courses.mock';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+    courses = COURSES;
+
+    getCourses(): Promise<any>{
+        return new Promise(resolve => {
+            resolve(this.courses);
+        });
+    }
+
+    getCousrse(courseId): Promise<any> {
+        const id = Number(courseId);
+        return new Promise(resolve =>
+            {
+                const course = this.courses.find(course => course.id === id);
+                if (!course) {
+                    throw new HttpException('Cou does not exist', 404);
+                }
+                resolve(course);
+            });
+    }
+
+    addCourse(course): Promise<any> {
+        return new Promise(resolve =>
+            {
+                this.courses.push(course);
+                resolve(this.courses);
+            });
+    }
+
+    deleteCourse(courseId): Promise<any> {
+        const id = Number(courseId);
+        return new Promise(resolve =>
+            {
+                const index = this.courses.findIndex(course => course.id === id);
+                if (index === -1){
+                    throw new HttpException('Course does not exist', 404);
+                }
+                this.courses.splice(index, 1);
+                resolve(this.courses);
+            })
+    }
+}
