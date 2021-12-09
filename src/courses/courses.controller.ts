f04fd60671f9b715b8cfb199867a33be1be0279b
Lotfi Arif <Luffyarif58@gmail.com>
2021-12-09 20:19:31 +0800

    Add Post and delete methods for courses

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Param, Post, Body, Delete, Query } from '@nestjs/common';
 import { CoursesService } from './courses.service';
+import { CreateCourseDto } from './create-course.dto';
 
 @Controller('courses')
 export class CoursesController {
@@ -15,5 +16,17 @@ export class CoursesController {
     async getCourse(@Param('courseId') courseId){
         const course = await this.coursesService.getCousrse(courseId);
         return course;
+    }
+
+    @Post()
+    async addCourse(@Body() createCourseDto: CreateCourseDto) {
+        const course = await this.coursesService.addCourse(createCourseDto);
+        return course;
+    }
+
+    @Delete()
+    async deleteCourse(@Query() query) {
+        const courses = await this.coursesService.deleteCourse(query.courseId);
+        return courses;
     }
 }
